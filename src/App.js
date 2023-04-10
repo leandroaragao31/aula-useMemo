e3e80c679cd63ab8df96f2dc7ddf20705314c08d
leandroaragao31 <leandroaragao31@gmail.com>
2023-04-10 02:59:08 -0300

    atualizando comentários

--- src/App.js
+++ src/App.js
@@ -9,32 +9,32 @@ const Caixa = styled.div`
 //ordem de estrutra da explicação 
 // 1º criar o component com 3 input type number.
 
-// 2º criar 3 states para setar o valor de cada input para ser atualizado conforme o state é alterado a conexão deverá ser feita com onChange
-// e value para mostrar o valor de cada state no input.
+ /* 2º criar 3 states para setar o valor de cada input para ser atualizado conforme o state é alterado a conexão deverá ser feita com onChange
+ e value para mostrar o valor de cada state no input.
+ */
+ /* 3º criar uma função fora do scopo da function App que vai retornar a soma de a + b,
+ em seguida criar uma variavel que altere o parametroda função de soma dos primeiros states criados para const result = soma(n1, n2);, executar essa função e mostrar para alunos que está funcionando normal
+ e o terceiro state será importante apenas para os proximos passos. */
 
-// 3º criar uma função fora do scopo da function App que vai retornar a soma de a + b,
-// em seguida criar uma variavel que altere o parametroda função de soma dos primeiros states criados para const result = soma(n1, n2);, executar essa função e mostrar para alunos que está funcionando normal
-// e o terceiro state será importante apenas para os proximos passos.
+/* 4º agora vamos criar uma logica dentro da função de soma para que a patir da data atual ele espere 2 segundos para excutar cada atualização de renderização sobre os input e resultado da soma.
+ Isso irá gerar um problema de rederização geral no componente pois essa logica ira criar um loop
+ e funcionar para todas as atualizações feitas na nossa pagina.
+ */
+/* 5º demostrar uma solução com ferramentas conhecidas como useState e useEffect usando da array de dependencia do useEffect
+ para setar apenas a rederização lenta para atualização dos states especificos n1 e n2. */
 
-//4º agora vamos criar uma logica dentro da função de soma para que a patir da data atual ele espere 2 segundos para excutar cada atualização de renderização sobre os input e resultado da soma.
-// Isso irá gerar um problema de rederização geral no componente pois essa logica ira criar um loop
-// e funcionar para todas as atualizações feitas na nossa pagina.
+/* 6º mostrar a solução com useMemo armazendo um valor pre calculado em e guardado em cache que só é executado quando as dependencias do useMemo forem modificadas,
+ funciona de uma forma bem semelhante ao useEffect,
+ a diferença é que o calculo ou a execução da função ja vai estar armazenada em cache evitando assim re-renderização do component
+ e chamando apenas o valor ou a execução quando as dependencias forem atualizadas ou modificadas.  */
 
-//5º demostrar uma solução com ferramentas conhecidas como useState e useEffect usando da array de dependencia do useEffect
-// para setar apenas a rederização lenta para atualização dos states especificos n1 e n2.
+/* 7º exemplo adicional simples mudando a cor de fundo da div com useMemo */
 
-//6º mostrar a solução com useMemo armazendo um valor pre calculado em e guardado em cache que só é executado quando as dependencias do useMemo forem modificadas,
-// funciona de uma forma bem semelhante ao useEffect,
-// a diferença é que o calculo ou a execução da função ja vai estar armazenada em cache evitando assim re-renderização do component
-// e chamando apenas o valor ou a execução quando as dependencias forem atualizadas ou modificadas. 
-
-//7º exemplo adicional simples mudando a cor de fundo da div com useMemo
-
-// OBS:Vale lembrar aqui que o useMemo não tem a mesma função do localStorage,
-// o useMemo é usado principalemnte para otimização de desempenho pois ao guardar uma execução em cache evita execuções repetidas
-// sempre que um componente é renderizado novamente.
-// Já o localStorage é um recurso que permite armazenar dados localmente no dispositivo do usuario 
-//(isso aqui é caso essa pergunta surga pelos alunos.)
+ /* OBS:Vale lembrar aqui que o useMemo não tem a mesma função do localStorage,
+ o useMemo é usado principalemnte para otimização de desempenho pois ao guardar uma execução em cache evita execuções repetidas
+ sempre que um componente é renderizado novamente.
+ Já o localStorage é um recurso que permite armazenar dados localmente no dispositivo do usuario 
+(isso aqui é caso essa pergunta surga pelos alunos.) */
 
 function soma(a, b) {
   const futuro = Date.now() + 800 /* aqui estou criando uma const para que espere 2000 mili segundos 
